4ed31c729315c7fd8f5de844b5f93a1c639e5638
Laura Lumbreras <lauralumbreras@Lauras-MacBook-Air.local>
2024-01-23 15:34:11 +0100

    giving structure

--- lessons/02-model-deployment/web_service/app_config.py
+++ lessons/02-model-deployment/web_service/app_config.py
@@ -1,5 +1,4 @@
 
-
 class AppConfig:
 
 # MODELS
--- lessons/02-model-deployment/web_service/lib/model_operations.py
+++ lessons/02-model-deployment/web_service/lib/model_operations.py
@@ -8,12 +8,6 @@ def load_pickle(path: str):
         loaded_obj = pickle.load(f)
     return loaded_obj
 
-def predict(input_data: Any, model_path: str = AppConfig.PATH_TO_PIPELINE):
-    model = load_pickle(model_path)
-    # Your prediction logic using the loaded model
-    #result = model.predict(input_data)
-    return model
-
 import pandas as pd
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import mean_squared_error
@@ -43,17 +37,17 @@ def extract_x_y(
 
 CATEGORICAL_COLS = ["PULocationID", "DOLocationID", "passenger_count"]
 
-def compute_target(
-    df: pd.DataFrame,
-    pickup_column: int = "PULocationID",
-    dropoff_column: int = "DOLocationID",
-) -> pd.DataFrame:
-    df["duration"] = df[dropoff_column] - df[pickup_column] /60
+#def compute_target(
+#    df: pd.DataFrame,
+#    pickup_column: int = "PULocationID",
+#    dropoff_column: int = "DOLocationID",
+#) -> pd.DataFrame:
+#    df["duration"] = df[dropoff_column] - df[pickup_column] /60
     #df["duration"] = df["duration"].dt.total_seconds() / 60
-    return df
+#    return df
 
-def filter_outliers(df: pd.DataFrame, min_duration: int = 1, max_duration: int = 60) -> pd.DataFrame:
-    return df[df["duration"].between(min_duration, max_duration)]
+#def filter_outliers(df: pd.DataFrame, min_duration: int = 1, max_duration: int = 60) -> pd.DataFrame:
+#    return df[df["duration"].between(min_duration, max_duration)]
 
 def encode_categorical_cols(df: pd.DataFrame, categorical_cols: List[str] = None) -> pd.DataFrame:
     if categorical_cols is None:
@@ -65,9 +59,12 @@ def encode_categorical_cols(df: pd.DataFrame, categorical_cols: List[str] = None
 
 def run_inference(data: Input):
     # Create a DataFrame with the necessary columns
+    model = load_pickle(AppConfig.PATH_TO_PIPELINE)
     input_df = pd.DataFrame([data.dict()])
-    input_df = compute_target(input_df)
+    #input_df = compute_target(input_df)
     input_df = encode_categorical_cols(input_df)
+    dict = input_df[CATEGORICAL_COLS].to_dict(orient="records")
+    X = input_df.transform(dict)
     extract_x_y(input_df)
-    result = predict(input_df)
+    result = model.predict(X)
     return result
